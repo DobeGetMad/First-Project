65f4129b70435c122a2469bd466be197da8e799e
DobeGetMad <ficho_dobeski@hotmail.com>
2021-05-03 22:45:20 +0200

    first commit

---
+++ Dog.java
@@ -0,0 +1,13 @@
+package GitHub;
+
+public class Dog extends Object{
+   public int age = 3;
+   public String name ="ekko";
+   public String tall ="50cm";
+   public String fat ="30kg";
+  	   
+      public void bark() {
+    	  System.out.println("wafwafwaf");
+	}
+
+}
